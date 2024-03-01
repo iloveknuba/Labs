7805eb950b0674789bbf8d795baedfd4695eb97a
iloveknuba <xxxcccer982@gmail.com>
2024-03-01 09:02:15 +0200

    rm com

--- lab3/index.js
+++ lab3/index.js
@@ -1,14 +1,14 @@
 let table = document.createElement("table");
 
-// Додавання рядків та клітинок до таблиці
+
 for (let i = 0; i < 30; i++) {
     let row = table.insertRow();
     for (let j = 0; j < 30; j++) {
         let cell = row.insertCell();
         cell.classList.add("black");
         cell.style.backgroundColor = "black";
-        cell.style.width="20px";// Встановлення білого кольору для клітинки
-        cell.style.height=" 20px";// Встановлення білого кольору для клітинки
+        cell.style.width="20px";
+        cell.style.height=" 20px";
         cell.addEventListener("click", cellClick);
 
     }
@@ -20,7 +20,7 @@ function cellClick(event) {
     cell.classList.toggle("black");
 }
 
-// Обробник події для <body> - змінює кольори всіх клітинок на протилежні
+
 document.body.addEventListener("click", function() {
     table.classList.toggle("invert-colors");
 });
@@ -28,5 +28,5 @@ document.body.addEventListener("click", function() {
 
 
 
-// Додавання таблиці до DOM
+
 document.body.appendChild(table);
